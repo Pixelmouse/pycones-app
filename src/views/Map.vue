a9573e4603bf0e651c3e4e106a6e123a413157fd
Valentina Rubane <mail@getrawfish.com>
2020-12-11 22:59:26 +0100

    map view

---
+++ src/views/Map.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="map">
+    <div>
+
+    </div>
+    <div class="map__legend">
+      <button class="map__legend--toggle">
+        <p>
+          Legenda
+        </p>
+        <div class="return">
+          &gt;
+        </div>
+      </button>
+      <ol class="map__legend--list">
+        <li class="map__legend--list-item">
+          <button>Lorem.</button>
+        </li>
+        <li class="map__legend--list-item">
+          <button>Animi.</button>
+        </li>
+        <li class="map__legend--list-item">
+          <button>Quam.</button>
+        </li>
+        <li class="map__legend--list-item">
+          <button>Asperiores.</button>
+        </li>
+        <li class="map__legend--list-item">
+          <button>Excepturi.</button>
+        </li>
+        <li class="map__legend--list-item">
+          <button>Beatae?</button>
+        </li>
+        <li class="map__legend--list-item">
+          <button>Hic?</button>
+        </li>
+      </ol>
+    </div>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Map'
+  }
+</script>
