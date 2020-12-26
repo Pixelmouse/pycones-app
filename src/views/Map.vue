9f7186a1ced6e290a0b3900099f0800413aeeb67
Valentina Rubane <mail@getrawfish.com>
2020-12-26 22:55:12 +0100

    leafletjs and stamen tiles added

--- src/views/Map.vue
+++ src/views/Map.vue
@@ -1,7 +1,20 @@
 <template>
-  <div class="map">
-    <div>
-
+  <div
+    class="map"
+    ref="mapContainer"
+  >
+    <div class="map__main">
+      <l-map
+        v-if="showMap"
+        :zoom="zoom"
+        :center="center"
+        :options="mapOptions"
+      >
+        <l-tile-layer
+          :url="url"
+          :attribution="attribution"
+        />
+      </l-map>
     </div>
     <div class="map__legend">
       <button
@@ -27,10 +40,7 @@
           v-if="showLegend"
           class="map__legend--list-wrapper"
         >
-          <ol
-            class="map__legend--list"
-            :style="{ gridTemplateRows: `repeat(${calcGridRows}, minmax(2rem, min-content))` }"
-          >
+          <ol class="map__legend--list">
             <li
               v-for="(location, index) in locations"
               :key="index"
@@ -38,7 +48,7 @@
               <button
                 class="map__legend--list-item"
               >
-                {{ index }}: {{ location }}
+                <span>{{ index }}:</span>{{ location }}
               </button>
             </li>
           </ol>
@@ -50,38 +60,48 @@
 
 <script>
 import Store from '@/store/index.ts'
+import { latLng } from 'leaflet'
+import { LMap, LTileLayer } from 'vue2-leaflet'
+import 'leaflet/dist/leaflet.css'
 
 export default {
   name: 'Map',
   store: Store,
+  components: {
+    LMap,
+    LTileLayer
+  },
   data () {
     return {
+      headerHeight: null,
+      showMap: false,
+      zoom: 13,
+      center: latLng(47.41322, -1.219482),
+      url: 'https://stamen-tiles.a.ssl.fastly.net/toner/{z}/{x}/{y}.png',
+      attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors',
+      mapOptions: {
+        zoomSnap: 0.5
+      },
       locations: [
-        'Lorem',
-        'Animi',
-        'Quam',
-        'Asperiores',
-        'Excepturi',
-        'Beatae',
-        'Hic',
-        'Lorem',
-        'Animi',
-        'Quam',
-        'Asperiores',
-        'Excepturi',
-        'Beatae',
-        'Hic'
+        'Lorem ipsum dolor sit',
+        'eriam fuga praesentium totam',
+        'Cum dolorem iure placeat',
+        'Assumenda culpa magni odio',
+        'Ab animi eaque suscipit',
+        'Eius minima quod vitae',
+        'Commodi dolores minus odit',
+        'Culpa fugiat laboriosam quos'
       ],
       showLegend: false
     }
   },
   beforeMount () {
     this.$store.commit('changeActiveMenu', 'map')
-  },
-  computed: {
-    calcGridRows: function () {
-      return Math.ceil(this.locations.length / 2)
-    }
+    this.headerHeight = document.querySelector('#header').scrollHeight
+    setTimeout(() => {
+      this.$refs.mapContainer.style.height = `calc(100% - 6rem - ${this.headerHeight}px)`
+      this.showMap = true
+    })
   },
   methods: {
     toggleLegend: function () {
